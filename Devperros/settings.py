c438c79982f460e6d4c123e697970f438d83d95b
jaquelyn <jaquelyncor7@gmail.com>
2021-08-25 22:16:47 -0600

    tablas añadidas

--- Devperros/settings.py
+++ Devperros/settings.py
@@ -75,15 +75,20 @@ WSGI_APPLICATION = 'Devperros.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Libros',
+        'USER': 'jaquelyn',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
+
 """
-import dj_database_url
+importdj_database_url
 from decouple import config
 
 DATABASES = {
@@ -91,8 +96,7 @@ DATABASES = {
         default=config('DATABASE_URL')
     )
 }
-
-
+"""
 
 
 # Password validation
--- apps/webperros/models.py
+++ apps/webperros/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class libro(models.Model):
+    pk_libro = models.AutoField(primary_key=True, null=False, blank=False)
+    nombre = models.CharField(max_length=50, null=False, blank=False)
+    autor = models.CharField(max_length=50, null=False, blank=False)
+    paginas = models.TextField(null=False,blank=False)
+    fecha_de_lanzamiento= models.DateField(null=False,auto_now = False , auto_now_add = False)
+    idioma= models.CharField(null=False, max_length=50, blank=False)
+    disponibilidad= models.CharField(null=False, max_length=50, blank=False)
+    precio= models.DecimalField(max_digits=2, decimal_places=2)
+
+class categorias(models.Model):
+    pk_categorias = models.AutoField(primary_key=True, null=False, blank=False)
+    variedad = models.CharField(max_length=80,null=False, blank=False)
+    editorial = models.CharField(max_length=80, null=False, blank=False)
+    fk_libro = models.ForeignKey(libro, null=False, blank=False, on_delete=models.CASCADE)
